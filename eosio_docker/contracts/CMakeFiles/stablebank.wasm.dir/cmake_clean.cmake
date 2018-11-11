7cd6b91faf5948cc6931372f98e2ff7c532fc000
Wei Guangcheng <twksos@qq.com>
2018-11-11 12:25:26 -0500

    init

---
+++ eosio_docker/contracts/CMakeFiles/stablebank.wasm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stablebank.wasm.dir/stablebank/stablebank.cpp.o"
+  "stablebank.wasm.pdb"
+  "stablebank.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stablebank.wasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
