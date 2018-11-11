229363997b9c75b6f4a9a7d61e6f812d3f164c83
Lin Yuan <yuanlin.dalian@gmail.com>
2018-11-11 11:02:10 -0800

    Working on PaymentScreen.

--- app/Screens/PaymentScreen.js
+++ app/Screens/PaymentScreen.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'native-base';
+import { StyleSheet, View, Image } from 'react-native';
+import {
+  Container,
+  Content,
+  Button,
+  Text,
+  Card,
+  CardItem,
+  Body,
+  Left,
+  Right,
+  DeckSwiper,
+  Thumbnail,
+  Icon,
+} from 'native-base';
 
 export default class PaymentScreen extends React.Component {
   static navigationOptions = {
@@ -9,9 +22,21 @@ export default class PaymentScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles}>
-        <Text>Detail Screen 1</Text>
-      </View>
+      <Container style={{ backgroundColor: '#1180bb' }}>
+        <Content padder contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Card style={{ borderRadius: 4, width: '90%' }}>
+            <CardItem style={{}}>
+              <Text>This is Payment Card</Text>
+            </CardItem>
+            <CardItem>
+              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+                <Image style={{ width: 200, height: 200 }}
+                       source={require('../assets/payments/bar-code.png')}/>
+              </View>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }
