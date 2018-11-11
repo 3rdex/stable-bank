532a796a93936cebc0b70e8e13aae5ca35680c4d
Lin Yuan <yuanlin.dalian@gmail.com>
2018-11-11 08:54:33 -0800

    Using native-base.

--- app/Screens/HomeScreen.js
+++ app/Screens/HomeScreen.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationActions, StackActions } from 'react-navigation';
+import { Container, Content, Button, Text } from 'native-base';
 
 export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home Screen',
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            this.props.navigation.dispatch(StackActions.reset({
-              index: 0,
-              actions: [
-                NavigationActions.navigate({ routeName: 'Detail' })
-              ],
-            }))
-          }}
-        />
-      </View>
+      <Container>
+        <Content>
+          <Button light><Text> Light </Text></Button>
+          <Button primary><Text> Primary </Text></Button>
+          <Button success><Text> Success </Text></Button>
+          <Button info><Text> Info </Text></Button>
+          <Button warning><Text> Warning </Text></Button>
+          <Button danger><Text> Danger </Text></Button>
+          <Button dark><Text> Dark </Text></Button>
+        </Content>
+      </Container>
     );
   }
 }
