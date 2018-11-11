53f6fc75b07655ddb46aeb73d9c41b46d9a3e0e4
Lin Yuan <yuanlin.dalian@gmail.com>
2018-11-11 09:19:32 -0800

    Working on main screen.

--- app/Screens/HomeScreen.js
+++ app/Screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Content, Button, Text } from 'native-base';
+import { Container, Content, Button, Text, Card, CardItem, Body, Left, Right } from 'native-base';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -10,10 +10,37 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <Container>
-        <Content>
-          <Button primary onPress={() => {
-            this.props.navigation.navigate('Detail')
-          }}><Text> Navigate To Detail Screen </Text></Button>
+        <Content padder>
+          <Card>
+            <CardItem header bordered>
+              <Text>NativeBase</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+              <Text>
+                NativeBase is a free and open source framework that enable
+                developers to build
+                high-quality mobile apps using React Native iOS and Android
+                apps
+                with a fusion of ES6.
+              </Text>
+              </Body>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Left>
+                <Button primary onPress={() => {
+                  this.props.navigation.navigate('Detail')
+                }}><Text> Send </Text></Button>
+              </Left>
+              <Right>
+                <Button primary onPress={() => {
+                  this.props.navigation.navigate('Detail')
+                }}><Text> Receive </Text></Button>
+              </Right>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
